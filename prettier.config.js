717f82fdf800193c798e7df619e7c672ae6e27c3
phenomen <phenomen@pm.me>
2025-02-13 15:13:03 -0600

    Restore

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-	useTabs: true,
-	singleQuote: true,
-	trailingComma: 'all',
-	printWidth: 100,
-	quoteProps: 'consistent',
-	singleAttributePerLine: true,
-};
